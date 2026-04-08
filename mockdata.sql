1496b1a7a6cc15e959a56beb0b92d741920c9966
simonequerzoli-boop <simone.querzoli@refresh-academy.org>
2026-04-08 11:26:55 +0200

    creazione tabelle mockdata

--- mockdata.sql
+++ mockdata.sql
@@ -20,3 +20,16 @@ insert into products (nome, articolo_peso, unita_collo, unita_bancale,
 codice_barre);
 values ('pomodoro preconfezionato 5 kg','true','1','6','1234567890128'),
 ('biscotti confezionati 500 grammi','false','6','8','1231231232')
+
+insert into giacenze (point_distribution_id, product_id, quantita, scadenza)
+values ('Saffi','12345789','6','-'),('Savena','987654321','48','-');
+
+insert into recipe (nome, descrizione, ingredienti)
+values ('maccheroni','maccheroni in bianco','pasta, olio, sale, parmigiano');
+ 
+insert into entities (nome, telefono,via)
+values ('Coldiretti','051-5789321','via Irnerio 45');
+
+insert into guests (nome, cognome, residente, data_nascita, numeri_famigliari
+professione, telefono, ente_segnalazione, ricevimento_pasto, pasti)
+values ('Giuseppe', 'Rezzonico','si', '27/05/1986', '5','panificatore','339151312', 'Caritas', 'in loco', '5');

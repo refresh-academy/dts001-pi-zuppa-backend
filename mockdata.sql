2b0cb80f22d39f1f99d39d58baafa008d568d18d
IreneCrocutaCrocuta <irene.ruscelli@refresh-academy.org>
2026-04-16 12:34:34 +0200

    popolate tabelle guests, entities, meal_types, guest_meal e guest_entity

--- mockdata.sql
+++ mockdata.sql
@@ -27,17 +27,37 @@ insert into recipes (nome, descrizione, ingredienti)
 values ('maccheroni al sugo','');
 
 insert into recipe_product values
-('maccheroni al sugo','pomodoro preconfezionato 5 kg', "0.08")
-('maccheroni al sugo','sale grosso 500g', "0.08")
-('maccheroni al sugo','maccheroni 1kg', "0.2")
+('maccheroni al sugo','pomodoro preconfezionato 5 kg', "0.08"),
+('maccheroni al sugo','sale grosso 500g', "0.08"),
+('maccheroni al sugo','maccheroni 1kg', "0.2"),
 
 
 insert into entities (nome, telefono,via)
-values ('Caritas','051-5789321','via Irnerio 45');
+values ('Caritas','051-5789321','via Irnerio 45'),
+('CSM', '051-223344', 'villa mazzacorati'),
+('ASP', '051-778899', 'via bianconiglio'),
+('Comune', '051-111111', 'via liberoparadisus');
 
-insert into guests (nome, cognome, residente, data_nascita, numeri_famigliari
-professione, telefono, ente_segnalazione, ricevimento_pasto, pasti)
-values ('Giuseppe', 'Rezzonico','si', '27/05/1986', '5','panificatore','339151312', 'Caritas', 'in loco', '5');
+insert into meal_types (tipo)
+values ('Standard'),
+('Vegetariano'),
+('Vegano'),
+('Halal'),
+('No latticini');
+
+insert into guests (nome, cognome, residente, data_nascita, numeri_famigliari,
+professione, telefono)
+values ('Giuseppe', 'Rezzonico','true', '1986-05-27', '5','panificatore','339151312');
+('Marco','Rossi','true','2001-11-03','1','disoccupato','051-225072');
+
+insert into guest_meal (guest_id,meal_type,ricevimento_pasto)
+values ('1','Standard','mensa'),
+('1','Standard','asporto'),
+('2','Vegetariano','mensa'),
+('2','Vegetariano','asporto');
+
+insert into guest_entity (guest_id, entity_id)
+values ('1','3'),('2','4');
 
 -- popoliamo utenti punti di distribuzione
 insert into user_site values 
